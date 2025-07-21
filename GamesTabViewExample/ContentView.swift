ce7963b85e29f7fd4781986ca7edc691928571a5
Gustavo Clemente <guh.clemente@gmail.com>
2025-07-21 20:30:52 -0300

    Initial Commit

---
+++ GamesTabViewExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GamesTabViewExample
+//
+//  Created by Guh F on 21/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GamesTabViewExample/GamesTabViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesTabViewExampleApp.swift
+//  GamesTabViewExample
+//
+//  Created by Guh F on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GamesTabViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
